3b8a36832d1c9d4b3ccf02be0771152463f23274
riet <adamriet@gmail.com>
2023-07-18 15:38:55 -0600

    updated signupform.js to support add_user

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -10,6 +10,7 @@ import {
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { GET_ME } from '../utils/queries.js'
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
